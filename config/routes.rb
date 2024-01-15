57f2db5904eae73e5a92c6b0f34ff257a91ac6a8
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-01-15 01:26:47 +0000

    最初の保存

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "homes#top"
+  resources :users, only: [:show]
+  resources :books, only: [:show, :create, :edit, :destroy, :index, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+#rails routes
+
+                                     #user GET    /users/:id(.:format)                                                                              users#show
+                                   #books GET    /books(.:format)                                                                                  books#index
+                                         #POST   /books(.:format)                                                                                  books#create
+                               #edit_book GET    /books/:id/edit(.:format)                                                                         books#edit
+                                    #book GET    /books/:id(.:format)                                                                              books#show
+                                         #DELETE /books/:id(.:format)                                                                              books#destroy
