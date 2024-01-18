08fa094e8c2b80b77cd81c703676214142e333bb
hiroki185 <68848005+hiroki185@users.noreply.github.com>
2024-01-18 01:44:22 +0000

    いったん保存

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
- def update
-
+def update
   @user = User.find(params[:id])
 
   if @user.update(user_params)
@@ -31,9 +30,12 @@ class UsersController < ApplicationController
   end
 end
 
+private
+
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+
 
 
 end
